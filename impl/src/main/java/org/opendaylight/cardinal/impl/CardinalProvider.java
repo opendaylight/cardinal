53a184564114a9c093bcaf36bf914f67131e548d
pande.gaurav <pande.gaurav@tcs.com>
2016-05-04 10:50:11 +0530

    Cardinal skeleton
    Change-Id: Iad85508b42df5c1fdebd93dfa55be46e550ec294
    Signed-off-by: pande.gaurav <pande.gaurav@tcs.com>

---
+++ impl/src/main/java/org/opendaylight/cardinal/impl/CardinalProvider.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2016 Tata Consultancy Services and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.cardinal.impl;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CardinalProvider implements BindingAwareProvider, AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CardinalProvider.class);
+
+    @Override
+    public void onSessionInitiated(ProviderContext session) {
+        LOG.info("CardinalProvider Session Initiated");
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOG.info("CardinalProvider Closed");
+    }
+
+}
---
+++ impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/cardinal/impl/rev141210/CardinalModule.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2016 Tata Consultancy Services and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cardinal.impl.rev141210;
+
+import org.opendaylight.cardinal.impl.CardinalProvider;
+
+public class CardinalModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cardinal.impl.rev141210.AbstractCardinalModule {
+    public CardinalModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public CardinalModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cardinal.impl.rev141210.CardinalModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        CardinalProvider provider = new CardinalProvider();
+        getBrokerDependency().registerProvider(provider);
+        return provider;
+    }
+
+}
