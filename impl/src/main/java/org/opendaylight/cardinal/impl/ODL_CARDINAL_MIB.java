04287d0472f147cf14f0cca9064d70771883b4d4
Vratko Polak <vrpolak@cisco.com>
2016-06-07 15:29:48 +0200

    Fixes and improvements upon 39921
    Change-Id: I9aab9ad32b70f8107efcbd607bdf1d907265b6f6
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- impl/src/main/java/org/opendaylight/cardinal/impl/ODL_CARDINAL_MIB.java
+++ impl/src/main/java/org/opendaylight/cardinal/impl/ODL_CARDINAL_MIB.java
@@ -31,216 +31,216 @@ import com.sun.management.snmp.agent.SnmpStandardObjectServer;
 @SuppressWarnings("all")
 public class ODL_CARDINAL_MIB extends SnmpMib implements Serializable {
 
-	/**
-	 * Default constructor. Initialize the Mib tree.
-	 */
-	public ODL_CARDINAL_MIB() {
-		mibName = "ODL_CARDINAL_MIB";
-	}
+  /**
+   * Default constructor. Initialize the Mib tree.
+   */
+  public ODL_CARDINAL_MIB() {
+    mibName = "ODL_CARDINAL_MIB";
+  }
 
-	/**
-	 * Initialization of the MIB with no registration in Java DMK.
-	 */
-	public void init() throws IllegalAccessException {
-		// Allow only one initialization of the MIB.
-		//
-		if (isInitialized == true) {
-			return;
-		}
+  /**
+   * Initialization of the MIB with no registration in Java DMK.
+   */
+  public void init() throws IllegalAccessException {
+    // Allow only one initialization of the MIB.
+    //
+    if (isInitialized == true) {
+      return;
+    }
 
-		try {
-			populate(null, null);
-		} catch (IllegalAccessException x) {
-			throw x;
-		} catch (RuntimeException x) {
-			throw x;
-		} catch (Exception x) {
-			throw new RuntimeException(x);
-		}
+    try {
+      populate(null, null);
+    } catch (IllegalAccessException x) {
+      throw x;
+    } catch (RuntimeException x) {
+      throw x;
+    } catch (Exception x) {
+      throw new RuntimeException(x);
+    }
 
-		isInitialized = true;
-	}
+    isInitialized = true;
+  }
 
-	/**
-	 * Initialization of the MIB with AUTOMATIC REGISTRATION in Java DMK.
-	 */
-	public ObjectName preRegister(MBeanServer server, ObjectName name) throws Exception {
-		// Allow only one initialization of the MIB.
-		//
-		if (isInitialized == true) {
-			throw new InstanceAlreadyExistsException();
-		}
+  /**
+   * Initialization of the MIB with AUTOMATIC REGISTRATION in Java DMK.
+   */
+  public ObjectName preRegister(MBeanServer server, ObjectName name) throws Exception {
+    // Allow only one initialization of the MIB.
+    //
+    if (isInitialized == true) {
+      throw new InstanceAlreadyExistsException();
+    }
 
-		// Initialize MBeanServer information.
-		//
-		this.server = server;
+    // Initialize MBeanServer information.
+    //
+    this.server = server;
 
-		populate(server, name);
+    populate(server, name);
 
-		isInitialized = true;
-		return name;
-	}
+    isInitialized = true;
+    return name;
+  }
 
-	/**
-	 * Initialization of the MIB with no registration in Java DMK.
-	 */
-	public void populate(MBeanServer server, ObjectName name) throws Exception {
-		// Allow only one initialization of the MIB.
-		//
-		if (isInitialized == true) {
-			return;
-		}
+  /**
+   * Initialization of the MIB with no registration in Java DMK.
+   */
+  public void populate(MBeanServer server, ObjectName name) throws Exception {
+    // Allow only one initialization of the MIB.
+    //
+    if (isInitialized == true) {
+      return;
+    }
 
-		if (objectserver == null)
-			objectserver = new SnmpStandardObjectServer();
+    if (objectserver == null)
+      objectserver = new SnmpStandardObjectServer();
 
-		// Initialization of the "OdlCardinalModule" group.
-		// To disable support of this group, redefine the
-		// "createOdlCardinalModuleMetaNode()" factory method, and make it
-		// return "null"
-		//
-		initOdlCardinalModule(server);
+    // Initialization of the "OdlCardinalModule" group.
+    // To disable support of this group, redefine the
+    // "createOdlCardinalModuleMetaNode()" factory method, and make it
+    // return "null"
+    //
+    initOdlCardinalModule(server);
 
-		isInitialized = true;
-	}
+    isInitialized = true;
+  }
 
-	// ------------------------------------------------------------
-	//
-	// Initialization of the "OdlCardinalModule" group.
-	//
-	// ------------------------------------------------------------
+  // ------------------------------------------------------------
+  //
+  // Initialization of the "OdlCardinalModule" group.
+  //
+  // ------------------------------------------------------------
 
-	/**
-	 * Initialization of the "OdlCardinalModule" group.
-	 * 
-	 * To disable support of this group, redefine the
-	 * "createOdlCardinalModuleMetaNode()" factory method, and make it return
-	 * "null"
-	 * 
-	 * @param server
-	 *            MBeanServer for this group (may be null)
-	 * 
-	 **/
-	protected void initOdlCardinalModule(MBeanServer server) throws Exception {
-		final String oid = getGroupOid("OdlCardinalModule", "1.3.6.1.3.1.1.1");
-		ObjectName objname = null;
-		if (server != null) {
-			objname = getGroupObjectName("OdlCardinalModule", oid, mibName + ":name=OdlCardinalModule");
-		}
-		final OdlCardinalModuleMeta meta = createOdlCardinalModuleMetaNode("OdlCardinalModule", oid, objname, server);
-		if (meta != null) {
-			meta.registerTableNodes(this, server);
+  /**
+   * Initialization of the "OdlCardinalModule" group.
+   * 
+   * To disable support of this group, redefine the
+   * "createOdlCardinalModuleMetaNode()" factory method, and make it return
+   * "null"
+   * 
+   * @param server
+   *            MBeanServer for this group (may be null)
+   * 
+   **/
+  protected void initOdlCardinalModule(MBeanServer server) throws Exception {
+    final String oid = getGroupOid("OdlCardinalModule", "1.3.6.1.3.1.1.1");
+    ObjectName objname = null;
+    if (server != null) {
+      objname = getGroupObjectName("OdlCardinalModule", oid, mibName + ":name=OdlCardinalModule");
+    }
+    final OdlCardinalModuleMeta meta = createOdlCardinalModuleMetaNode("OdlCardinalModule", oid, objname, server);
+    if (meta != null) {
+      meta.registerTableNodes(this, server);
 
-			// Note that when using standard metadata,
-			// the returned object must implement the "OdlCardinalModuleMBean"
-			// interface.
-			//
-			final OdlCardinalModuleMBean group = (OdlCardinalModuleMBean) createOdlCardinalModuleMBean(
-					"OdlCardinalModule", oid, objname, server);
-			meta.setInstance(group);
-			registerGroupNode("OdlCardinalModule", oid, objname, meta, group, server);
-		}
-	}
+      // Note that when using standard metadata,
+      // the returned object must implement the "OdlCardinalModuleMBean"
+      // interface.
+      //
+      final OdlCardinalModuleMBean group = (OdlCardinalModuleMBean) createOdlCardinalModuleMBean(
+          "OdlCardinalModule", oid, objname, server);
+      meta.setInstance(group);
+      registerGroupNode("OdlCardinalModule", oid, objname, meta, group, server);
+    }
+  }
 
-	/**
-	 * Factory method for "OdlCardinalModule" group metadata class.
-	 * 
-	 * You can redefine this method if you need to replace the default generated
-	 * metadata class with your own customized class.
-	 * 
-	 * @param groupName
-	 *            Name of the group ("OdlCardinalModule")
-	 * @param groupOid
-	 *            OID of this group
-	 * @param groupObjname
-	 *            ObjectName for this group (may be null)
-	 * @param server
-	 *            MBeanServer for this group (may be null)
-	 * 
-	 * @return An instance of the metadata class generated for the
-	 *         "OdlCardinalModule" group (OdlCardinalModuleMeta)
-	 * 
-	 **/
-	protected OdlCardinalModuleMeta createOdlCardinalModuleMetaNode(String groupName, String groupOid,
-			ObjectName groupObjname, MBeanServer server) {
-		return new OdlCardinalModuleMeta(this, objectserver);
-	}
+  /**
+   * Factory method for "OdlCardinalModule" group metadata class.
+   * 
+   * You can redefine this method if you need to replace the default generated
+   * metadata class with your own customized class.
+   * 
+   * @param groupName
+   *            Name of the group ("OdlCardinalModule")
+   * @param groupOid
+   *            OID of this group
+   * @param groupObjname
+   *            ObjectName for this group (may be null)
+   * @param server
+   *            MBeanServer for this group (may be null)
+   * 
+   * @return An instance of the metadata class generated for the
+   *         "OdlCardinalModule" group (OdlCardinalModuleMeta)
+   * 
+   **/
+  protected OdlCardinalModuleMeta createOdlCardinalModuleMetaNode(String groupName, String groupOid,
+      ObjectName groupObjname, MBeanServer server) {
+    return new OdlCardinalModuleMeta(this, objectserver);
+  }
 
-	/**
-	 * Factory method for "OdlCardinalModule" group MBean.
-	 * 
-	 * You can redefine this method if you need to replace the default generated
-	 * MBean class with your own customized class.
-	 * 
-	 * @param groupName
-	 *            Name of the group ("OdlCardinalModule")
-	 * @param groupOid
-	 *            OID of this group
-	 * @param groupObjname
-	 *            ObjectName for this group (may be null)
-	 * @param server
-	 *            MBeanServer for this group (may be null)
-	 * 
-	 * @return An instance of the MBean class generated for the
-	 *         "OdlCardinalModule" group (OdlCardinalModule)
-	 * 
-	 *         Note that when using standard metadata, the returned object must
-	 *         implement the "OdlCardinalModuleMBean" interface.
-	 **/
-	protected Object createOdlCardinalModuleMBean(String groupName, String groupOid, ObjectName groupObjname,
-			MBeanServer server) {
+  /**
+   * Factory method for "OdlCardinalModule" group MBean.
+   * 
+   * You can redefine this method if you need to replace the default generated
+   * MBean class with your own customized class.
+   * 
+   * @param groupName
+   *            Name of the group ("OdlCardinalModule")
+   * @param groupOid
+   *            OID of this group
+   * @param groupObjname
+   *            ObjectName for this group (may be null)
+   * @param server
+   *            MBeanServer for this group (may be null)
+   * 
+   * @return An instance of the MBean class generated for the
+   *         "OdlCardinalModule" group (OdlCardinalModule)
+   * 
+   *         Note that when using standard metadata, the returned object must
+   *         implement the "OdlCardinalModuleMBean" interface.
+   **/
+  protected Object createOdlCardinalModuleMBean(String groupName, String groupOid, ObjectName groupObjname,
+      MBeanServer server) {
 
-		// Note that when using standard metadata,
-		// the returned object must implement the "OdlCardinalModuleMBean"
-		// interface.
-		//
-		if (server != null)
-			return new OdlCardinalModule(this, server);
-		else
-			return new OdlCardinalModule(this);
-	}
+    // Note that when using standard metadata,
+    // the returned object must implement the "OdlCardinalModuleMBean"
+    // interface.
+    //
+    if (server != null)
+      return new OdlCardinalModule(this, server);
+    else
+      return new OdlCardinalModule(this);
+  }
 
-	// ------------------------------------------------------------
-	//
-	// Implements the "registerTableMeta" method defined in "SnmpMib".
-	// See the "SnmpMib" Javadoc API for more details.
-	//
-	// ------------------------------------------------------------
-	@Override
-	@SuppressWarnings("unchecked")
-	public void registerTableMeta(String name, SnmpMibTable meta) {
-		if (metadatas == null)
-			return;
-		if (name == null)
-			return;
-		metadatas.put(name, meta);
-	}
+  // ------------------------------------------------------------
+  //
+  // Implements the "registerTableMeta" method defined in "SnmpMib".
+  // See the "SnmpMib" Javadoc API for more details.
+  //
+  // ------------------------------------------------------------
+  @Override
+  @SuppressWarnings("unchecked")
+  public void registerTableMeta(String name, SnmpMibTable meta) {
+    if (metadatas == null)
+      return;
+    if (name == null)
+      return;
+    metadatas.put(name, meta);
+  }
 
-	// ------------------------------------------------------------
-	//
-	// Implements the "getRegisteredTableMeta" method defined in "SnmpMib".
-	// See the "SnmpMib" Javadoc API for more details.
-	//
-	// ------------------------------------------------------------
+  // ------------------------------------------------------------
+  //
+  // Implements the "getRegisteredTableMeta" method defined in "SnmpMib".
+  // See the "SnmpMib" Javadoc API for more details.
+  //
+  // ------------------------------------------------------------
 
-	public SnmpMibTable getRegisteredTableMeta(String name) {
-		if (metadatas == null)
-			return null;
-		if (name == null)
-			return null;
-		return (SnmpMibTable) metadatas.get(name);
-	}
+  public SnmpMibTable getRegisteredTableMeta(String name) {
+    if (metadatas == null)
+      return null;
+    if (name == null)
+      return null;
+    return (SnmpMibTable) metadatas.get(name);
+  }
 
-	public SnmpStandardObjectServer getStandardObjectServer() {
-		if (objectserver == null)
-			objectserver = new SnmpStandardObjectServer();
-		return objectserver;
-	}
+  public SnmpStandardObjectServer getStandardObjectServer() {
+    if (objectserver == null)
+      objectserver = new SnmpStandardObjectServer();
+    return objectserver;
+  }
 
-	private boolean isInitialized = false;
+  private boolean isInitialized = false;
 
-	protected SnmpStandardObjectServer objectserver;
+  protected SnmpStandardObjectServer objectserver;
 
-	@SuppressWarnings("rawtypes")
-	protected final Hashtable metadatas = new Hashtable();
+  @SuppressWarnings("rawtypes")
+  protected final Hashtable metadatas = new Hashtable();
 }
--- impl/src/main/java/org/opendaylight/cardinal/impl/OdlCardinalImpl.java
+++ impl/src/main/java/org/opendaylight/cardinal/impl/OdlCardinalImpl.java
@@ -17,47 +17,47 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.LoggerFactory;
 
 public class OdlCardinalImpl implements AutoCloseable {
-	private DataBroker dataProvider;
-	private final static org.slf4j.Logger LOG = LoggerFactory.getLogger(OdlCardinalImpl.class);
-	public static final InstanceIdentifier<CardinalSystemInfo> Cardinal_IID = InstanceIdentifier
-			.builder(CardinalSystemInfo.class).build();
+  private DataBroker dataProvider;
+  private final static org.slf4j.Logger LOG = LoggerFactory.getLogger(OdlCardinalImpl.class);
+  public static final InstanceIdentifier<CardinalSystemInfo> Cardinal_IID = InstanceIdentifier
+      .builder(CardinalSystemInfo.class).build();
 
-	public OdlCardinalImpl() {
-		// TODO Auto-generated constructor stub
+  public OdlCardinalImpl() {
+    // TODO Auto-generated constructor stub
 
-	}
+  }
 
-	public void setDataProvider(final DataBroker salDataProvider) {
-		this.dataProvider = salDataProvider;
-		LOG.info("set data broker");
-	}
+  public void setDataProvider(final DataBroker salDataProvider) {
+    this.dataProvider = salDataProvider;
+    LOG.info("set data broker");
+  }
 
-	public void setValues() {
-		OdlCardinalGet get = new OdlCardinalGet();
-		String sysInfo = get.snmpGet("localhost", "public", "1.3.6.1.3.1.1.1.6.0");
-		String sysHostAddress = get.snmpGet("localhost", "public", "1.3.6.1.3.1.1.1.3.0");
-		String sysCpuUsage = get.snmpGet("localhost", "public", "1.3.6.1.3.1.1.1.1.0");
-		String sysMemUsage = get.snmpGet("localhost", "public", "1.3.6.1.3.1.1.1.2.0");
-		String sysUptime = get.snmpGet("localhost", "public", "1.3.6.1.3.1.1.1.5.0");
-		LOG.info("wrting data to operational datastore");
-		CardinalSystemInfoBuilder v = new CardinalSystemInfoBuilder();
-		v.setOdlSystemSysInfo(sysInfo);
-		v.setOdlSystemHostAddress(sysHostAddress);
-		v.setOdlSystemCpuUsage(sysCpuUsage);
-		v.setOdlSystemMemUsage(sysMemUsage);
-		v.setOdlSystemOdlUptime(sysUptime);
-		LOG.info("initiating read write transaction");
-		ReadWriteTransaction txn = dataProvider.newReadWriteTransaction();
-		txn.put(LogicalDatastoreType.OPERATIONAL, Cardinal_IID, v.build());
-		// txn.put(LogicalDatastoreType.CONFIGURATION, PRIMES_IID, p1);
-		txn.submit();
+  public void setValues() {
+    OdlCardinalGet get = new OdlCardinalGet();
+    String sysInfo = get.snmpGet("localhost", "public", "1.3.6.1.3.1.1.1.6.0");
+    String sysHostAddress = get.snmpGet("localhost", "public", "1.3.6.1.3.1.1.1.3.0");
+    String sysCpuUsage = get.snmpGet("localhost", "public", "1.3.6.1.3.1.1.1.1.0");
+    String sysMemUsage = get.snmpGet("localhost", "public", "1.3.6.1.3.1.1.1.2.0");
+    String sysUptime = get.snmpGet("localhost", "public", "1.3.6.1.3.1.1.1.5.0");
+    LOG.info("wrting data to operational datastore");
+    CardinalSystemInfoBuilder v = new CardinalSystemInfoBuilder();
+    v.setOdlSystemSysInfo(sysInfo);
+    v.setOdlSystemHostAddress(sysHostAddress);
+    v.setOdlSystemCpuUsage(sysCpuUsage);
+    v.setOdlSystemMemUsage(sysMemUsage);
+    v.setOdlSystemOdlUptime(sysUptime);
+    LOG.info("initiating read write transaction");
+    ReadWriteTransaction txn = dataProvider.newReadWriteTransaction();
+    txn.put(LogicalDatastoreType.OPERATIONAL, Cardinal_IID, v.build());
+    // txn.put(LogicalDatastoreType.CONFIGURATION, PRIMES_IID, p1);
+    txn.submit();
 
-	}
+  }
 
-	@Override
-	public void close() throws Exception {
-		// TODO Auto-generated method stub
+  @Override
+  public void close() throws Exception {
+    // TODO Auto-generated method stub
 
-	}
+  }
 
 }
