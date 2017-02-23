7b8193f78bb8220e9f7c3f4f37ef128a89563d96
Anirban Roy <anirban.roy@ericsson.com>
2017-02-23 12:00:33 +0530

    These changes are done to provide Cardinal as an "SNMP Agent as a Service" 1. An Interface "SnmpMIBService" is defined in the API bundle, which provides services like,    a. Load a third party MIB    b. Unload a previously loaded MIB    c. Send a TRAP message.
    2. The Agent class provides the "impl" for "SnmpMIBService"
    3. MIBs to be loaded are to be build using "mibgen" tool from the MIB text
    4. A file based SNMP manager IP ACL is added, which could be used to keep defaut trap address.
    
    No bundle or bundle context during build test
    API bundle depends on jdmk, change in feature.xml
    
    Change-Id: If413a82cf24dfab2a392331354d540ffed536adb
    Signed-off-by: Anirban Roy <royanirban76@gmail.com>

---
+++ api/src/main/java/SnmpMibService.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2015 TCS and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.cardinal.api;
+
+import java.net.InetAddress;
+import java.util.Enumeration;
+
+import com.sun.management.snmp.SnmpOid;
+import com.sun.management.snmp.SnmpString;
+import com.sun.management.snmp.agent.SnmpMibAgent;
+
+public interface SnmpMibService {
+    /*
+     * Load your custom MIB
+     */
+    void loadMib(final SnmpMibAgent mib);
+
+    /*
+     * Unload your custom MIB
+     */
+    void unloadMib(final SnmpMibAgent mib);
+
+    /**
+     * Set a trap address
+     */
+    void setTrapAddress(final InetAddress trapReceiver);
+
+    /*
+     * Send a TRAP
+     */
+    void sendSnmpTrap(SnmpOid ntOid, Enumeration<SnmpOid> payloadOid, Enumeration<SnmpString> payloadData);
+}
